498f2975a942b4158046572b7d13ccadc5e5e709
SenhaiQu <qusenhai2@gmail.com>
2022-04-30 21:03:45 -0400

    HelloWorld Changed

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,8 +1,44 @@
+import java.util.Scanner;
 
 public class HelloWorld {
 	public static void main(String args[]) {
+		Scanner myObj=new Scanner(System.in);
 		System.out.print("Hellow World");
-		System.out.print("Hellow World2");
+		System.out.println("Type an integer: ");
+		int typeNumber=myObj.nextInt();
+		boolean isNumber=myObj.hasNextInt();
+		System.out.println("Numver is :"+typeNumber);
+		
+		//for foreach , while, case swich
+		
+		//print 0 to 10;
+		//1 while
+		//2 for
+		//3 foreach
+		
+		//Array Examples
+		int[] intArray=new int[3];
+		intArray[0]=2;
+		intArray[1]=2;
+		intArray[2]=2;
+		intArray[3]=2; //----index outof bundled
+		
+		//O(1) big O worst case bundled;
+		//O(n) grow as n, propshally   
+		//O(n^2) grow as n^2
+		
+		//Data Structure 
+		
+		//Fib Recusive
+		
+		//Fizbuzz
+		
+		//Java Rejex
+		
+		// Java Gabarge colletion
+		
+		// try catch finally close collection
+		
 	}
 	
 
